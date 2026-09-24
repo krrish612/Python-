c5c42be2f90f63f1aa39145fd9e01ec1231d566d
Krrish Tripathi <tripathikrrish172@gmail.com>
2026-09-24 19:50:05 +0530

    tiny exercise

---
+++ TinyExercies.py
@@ -0,0 +1,9 @@
+name = input("What is your name :")
+age = 18
+height = input("How tall are you in (CM):")
+Stud = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(Stud))
